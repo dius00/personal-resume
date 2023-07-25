7107d47ccba221d339de83b6eb6c235e87fee835
Antonio De Rinaldis <antonio.derinaldis@outlook.com>
2023-07-25 18:06:57 +0200

    updated ElevatedButtonThemeData

--- personal_resume/lib/interface/configuration/app_theme.dart
+++ personal_resume/lib/interface/configuration/app_theme.dart
@@ -2,17 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:personal_resume/interface/configuration/custom_color.dart';
 
 ThemeData appTheme = ThemeData(
-    buttonTheme: _buttonTheme,
-    colorScheme: _appColorScheme,
-    useMaterial3: true,
-    extensions: [_purpleCustomColor]);
+  elevatedButtonTheme: _elevatedButtonThemeData,
+  colorScheme: _appColorScheme,
+  useMaterial3: true,
+  extensions: [_purpleCustomColor],
+);
+
+ElevatedButtonThemeData _elevatedButtonThemeData = ElevatedButtonThemeData(
+  style: _buttonStyle.copyWith(
+    shape: MaterialStateProperty.all(
+      RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+    ),
+    elevation: MaterialStateProperty.all(3),
+    shadowColor: MaterialStateProperty.all(
+      const Color(0xFF333333).withOpacity(0.4),
+    ),
+    enableFeedback: true,
+  ),
+);
 
 const _appColorScheme = ColorScheme(
   primary: Color(0xFF1e90ff),
   secondary: Color(0xFFffab1e),
   tertiary: Color(0xFF7fff62),
   surface: Color(0xFF333333),
-  background: Colors.black,
+  background: Colors.white,
   error: Color(0xFFB00020),
   brightness: Brightness.dark,
   onPrimary: Colors.white,
@@ -22,14 +38,10 @@ const _appColorScheme = ColorScheme(
   onSurface: Colors.white,
 );
 
-const _buttonTheme = ButtonThemeData(
-  buttonColor: Color(0xFF333333),
-  textTheme: ButtonTextTheme.normal,
-  shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.all(
-      Radius.circular(15),
-    ),
-  ),
+ButtonStyle _buttonStyle = ButtonStyle(
+  backgroundColor: MaterialStateProperty.all(const Color(0xFF333333)),
+  foregroundColor: MaterialStateProperty.all(Colors.white),
+  alignment: Alignment.center,
 );
 
 abstract class Purple {}
