8b92650b638b8b8aac7ad5ff9408ca7a4d7f6234
Antonio De Rinaldis <antonio.derinaldis@outlook.com>
2023-07-25 10:22:42 +0200

    rename to app_theme, make variable private

--- personal_resume/lib/interface/configuration/app_theme.dart
+++ personal_resume/lib/interface/configuration/app_theme.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:personal_resume/interface/configuration/custom_color.dart';
 
-import 'custom_color.dart';
+ThemeData appTheme = ThemeData(
+    buttonTheme: _buttonTheme,
+    colorScheme: _appColorScheme,
+    useMaterial3: true,
+    extensions: [_purpleCustomColor]);
 
-const appColorScheme = ColorScheme(
+const _appColorScheme = ColorScheme(
   primary: Color(0xFF1e90ff),
   secondary: Color(0xFFffab1e),
   tertiary: Color(0xFF7fff62),
@@ -17,9 +22,19 @@ const appColorScheme = ColorScheme(
   onSurface: Colors.white,
 );
 
+const _buttonTheme = ButtonThemeData(
+  buttonColor: Color(0xFF333333),
+  textTheme: ButtonTextTheme.normal,
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(15),
+    ),
+  ),
+);
+
 abstract class Purple {}
 
-CustomColor<Purple> purpleCustomColor = const CustomColor<Purple>(
+CustomColor<Purple> _purpleCustomColor = const CustomColor<Purple>(
   customColor: Color(0xFFbc62ff),
   onCustomColor: Colors.white,
   customColorContainer: Color(0xFFbc62ff),
